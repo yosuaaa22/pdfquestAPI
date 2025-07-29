d1bbe12f1d19f434815f4ace507ed8564a3851f6
Athala Angwyn Renaldi <email.angwynren@gmail.com>
2025-07-29 15:12:39 +0700

    fix(Dtos) : yellow

--- Dtos/Create_penyedia_layanan.cs
+++ Dtos/Create_penyedia_layanan.cs
@@ -6,34 +6,34 @@ namespace pdfquestAPI.Dtos
     public class CreatePenyediaLayananDto
     {
         [Required]
-        public string NamaEntitasCalonProvider { get; set; }
+        public required  string NamaEntitasCalonProvider { get; set; }
         public string? JenisPerusahaan { get; set; }
         [Required]
-        public string NoNibPihakKedua { get; set; }
+        public required string NoNibPihakKedua { get; set; }
         public string? AlamatPemegangPolis { get; set; }
         [Required]
-        public string NamaPerwakilan { get; set; }
+        public required  string NamaPerwakilan { get; set; }
         public string? JabatanPerwakilan { get; set; }
         [Required]
-        public string JenisFasilitas { get; set; }
+        public required  string JenisFasilitas { get; set; }
         [Required]
-        public string NamaFasilitasKesehatan { get; set; }
+        public required  string NamaFasilitasKesehatan { get; set; }
         public string? NamaDokumenIzin { get; set; }
         public string? NomorDokumenIzin { get; set; }
         public DateTime? TanggalDokumenIzin { get; set; }
         public string? InstansiPenerbitIzin { get; set; }
         [Required]
-        public string NamaBank { get; set; }
+        public required string NamaBank { get; set; }
         public string? NamaCabangBank { get; set; }
         [Required]
-        public string NomorRekening { get; set; }
+        public required  string NomorRekening { get; set; }
         [Required]
-        public string PemilikRekening { get; set; }
+        public required string PemilikRekening { get; set; }
         [Required]
-        public string NamaPemilikNpwp { get; set; }
+        public required string NamaPemilikNpwp { get; set; }
         [Required]
-        public string NoNpwp { get; set; }
+        public required string NoNpwp { get; set; }
         [Required]
-        public string JenisNpwp { get; set; }
+        public required  string JenisNpwp { get; set; }
     }
 }
